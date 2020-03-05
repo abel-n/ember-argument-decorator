e9cab20fb9d071e84ed4854b866ed2a1815dc1bd
Ábel Nyéki <abel.nyeki@gmail.com>
2020-03-05 13:49:50 +0100

    remove temporary demo component

--- tests/dummy/app/components/demo/component.js
+++
@@ -1,6 +0,0 @@
-import Component from '@glimmer/component';
-import argument from 'ember-argument-decorator';
-
-export default class DemoComponent extends Component {
-  @argument name = 'Travis Rice'
-}
